0efa69c1be6884152011bc563a81917cc7eec3cb
Michael Scriven <bigscriv03@gmail.com>
2015-01-14 13:25:35 -0500

    Added user to movies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114164353) do
+ActiveRecord::Schema.define(version: 20150114175728) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150114164353) do
     t.string   "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
