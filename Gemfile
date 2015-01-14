f4c71471db3d5924f9f2d601b58a88f37804a3be
Michael Scriven <bigscriv03@gmail.com>
2015-01-14 14:28:11 -0500

    Added carrierwave gem

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'sqlite3'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
-
+gem 'carrierwave'
 gem 'devise', '~> 3.4.1'
 gem 'jquery-rails'
 gem 'turbolinks'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114175728) do
+ActiveRecord::Schema.define(version: 20150114192719) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150114175728) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
