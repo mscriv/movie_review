33d24522bf143a09bacc80c016a70b0119399bf7
Michael Scriven <bigscriv03@gmail.com>
2015-01-14 20:24:17 -0500

    Created reviews scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150114192719) do
+ActiveRecord::Schema.define(version: 20150115012326) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20150114192719) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
